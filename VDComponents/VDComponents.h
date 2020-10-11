976c299770fab39b52ceede7bd656cba90bab44c
Bia <biaplutarco@gmail.com>
2020-10-11 16:51:02 -0300

    Initial Commit

---
+++ VDComponents/VDComponents.h
@@ -0,0 +1,18 @@
+//
+//  VDComponents.h
+//  VDComponents
+//
+//  Created by Bia Plutarco on 11/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VDComponents.
+FOUNDATION_EXPORT double VDComponentsVersionNumber;
+
+//! Project version string for VDComponents.
+FOUNDATION_EXPORT const unsigned char VDComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VDComponents/PublicHeader.h>
+
+
