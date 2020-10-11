78925985af239e7984c9e760a16201c2490f80ef
Bia <biaplutarco@gmail.com>
2020-10-11 17:15:03 -0300

    Adicionado app colors

---
+++ VDComponents/Source/Extensions/UIColor+AppColors.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor+AppColors.swift
+//  VDComponents
+//
+//  Created by Bia Plutarco on 11/10/20.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    struct AppColors {
+        
+        static let orange = UIColor.init(named: "Orange")!
+        static let lightOrange = UIColor.init(named: "LightOrange")!
+        static let white = UIColor.init(named: "White")!
+        static let black = UIColor.init(named: "Black")!
+        static let darkGray = UIColor.init(named: "DarkGray")!
+        static let gray = UIColor.init(named: "Gray")!
+    }
+}
+
