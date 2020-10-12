c005af4d0d4be2c1407d44ff7559f8f4016b7080
Bia <biaplutarco@gmail.com>
2020-10-12 14:05:11 -0300

    Criado line stack view

---
+++ VDComponents/Source/Components/StackView/VDStackViewDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  LineStackViewDelegate.swift
+//  VDComponents
+//
+//  Created by Bia Plutarco on 12/10/20.
+//
+
+import UIKit
+
+protocol VDStackViewDelegate: class {
+    func didChangeTo(index: Int)
+}
